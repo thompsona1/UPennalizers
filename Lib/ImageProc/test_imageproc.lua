41036b81bbf3173d494a694a16ea68283dc58844
thompsona1 <thompsona1@gmail.com>
2011-09-18 16:06:01 -0300

    Lua comments!

--- Lib/ImageProc/test_imageproc.lua
+++ Lib/ImageProc/test_imageproc.lua
@@ -1,16 +1,16 @@
 require('ImageProc');
-require('carray'); //color array?
+require('carray'); --color array?
 
 cdt = carray.new('c', 262144);
-pcdt = carray.pointer(cdt); //pointer of cdt - but what's cdt?
+pcdt = carray.pointer(cdt); --pointer of cdt - but what's cdt?
 
-width = 320; //this is half-VGA resolution
+width = 320; --this is half-VGA resolution
 height = 240;
 
-rgb = carray.new('c', 3*width*height); //red green blue
-prgb = carray.pointer(rgb); //pointer of rgb
+rgb = carray.new('c', 3*width*height); --red green blue
+prgb = carray.pointer(rgb); --pointer of rgb
 
-/*
+--[[
 Below looks like YUV colorspace, alternative to RGB.
 From RGB to YUV
 Y = 0.299R + 0.587G + 0.114B
@@ -25,7 +25,7 @@ R = Y + 1.140V
 G = Y - 0.395U - 0.581V
 B = Y + 2.032U
 from http://www.pcmag.com/encyclopedia_term/0,2542,t=YUVRGB+conversion+formulas&i=55166,00.asp
-*/
+--]]
 
 pyuyv = ImageProc.rgb_to_yuyv(prgb, width*height);
 yuyv = carray.cast(pyuyv, 'i', width*height);
